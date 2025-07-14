6cb2ac369d151c20df089140bf41a8b859d9309d
Sharath3002 <sharathmh2604@gmail.com>
2025-07-14 14:31:53 +0530

    Add Prometheus and Docker Compose setup

--- app.py
+++ app.py
@@ -38,4 +38,5 @@ def products():
     return render_template('products.html', products=PRODUCTS, username=username)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
